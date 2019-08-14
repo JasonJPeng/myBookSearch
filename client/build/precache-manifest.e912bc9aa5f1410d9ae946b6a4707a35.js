6d84ce16a89a376eb2982456e584597d61692620
Jason Peng <jason.j.peng@gamil.com>
2019-08-13 21:21:35 -0700

    build

---
+++ client/build/precache-manifest.e912bc9aa5f1410d9ae946b6a4707a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a918203326f8256592e",
+    "url": "/static/js/main.7bbf3f57.chunk.js"
+  },
+  {
+    "revision": "d1feded6237606d82bed",
+    "url": "/static/js/2.f301fa3b.chunk.js"
+  },
+  {
+    "revision": "2a918203326f8256592e",
+    "url": "/static/css/main.2a453acb.chunk.css"
+  },
+  {
+    "revision": "9da2fe2cc911002586729e2260c5418a",
+    "url": "/index.html"
+  }
+];
